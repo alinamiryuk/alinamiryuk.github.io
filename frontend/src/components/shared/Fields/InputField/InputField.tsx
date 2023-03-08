4a5c356ef7c3eba3d82873f1d1c2bcc6454986f2
alinamiryuk <alinamiryuk@mail.ru>
2023-03-08 16:03:05 +0300

    debounce

--- frontend/src/components/shared/Fields/InputField/InputField.tsx
+++ frontend/src/components/shared/Fields/InputField/InputField.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { ReactComponent as EyeOpen } from 'assets/icons/eye-open.svg'
 import { ReactComponent as EyeClosed } from 'assets/icons/eye-closed.svg'
+import { useDebounce } from 'lib/hooks/useDebounce'
 import { LabeledField } from 'components/shared/Fields/LabeledField/LabeledField'
 import { IInputFieldProps } from './InputFieldTypes'
 import './InputField.css'
@@ -15,12 +16,13 @@ export const InputField: React.FC<IInputFieldProps> = ({
   onChange,
 }) => {
   const [isVisible, setIsVisible] = React.useState(false)
+  const debouncedIsValid = useDebounce(isValid, 1000)
   const isPassword = type === 'password'
   const passwordFieldType = isVisible ? 'text' : 'password'
   const inputType = isPassword ? passwordFieldType : type
   const visibilityController = () => setIsVisible(!isVisible)
-  const passwordClassName = `input-field input-field-${
-    !!errorMessage ? 'error' : ''
+  const inputClassName = `input-field input-field-${
+    !!errorMessage && !debouncedIsValid ? 'error' : ''
   }`
 
   return (
@@ -28,10 +30,10 @@ export const InputField: React.FC<IInputFieldProps> = ({
       label={label}
       errorMessage={errorMessage}
       isPassword={isPassword}
-      isValid={isValid}
+      isValid={debouncedIsValid}
     >
       <input
-        className={passwordClassName}
+        className={inputClassName}
         placeholder={placeholder}
         type={inputType}
         value={value}
